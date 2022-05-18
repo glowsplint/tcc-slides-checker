042dccfeae92ab85419bf5402e4016f5f7f2925c
glowsplint <tym.teo.23@gmail.com>
2022-05-18 15:37:55 +0800

    Add test_section_headers_have_correct_order

--- tests/test_content.py
+++ tests/test_content.py
@@ -5,13 +5,19 @@ import pytest
 from pptx import Presentation
 from pptx.slide import Slide
 
+if __name__ == "__main__":
+    import os
+
+    if Path(os.getcwd()).name == "tests":
+        os.chdir("..")
+
 
 @pytest.fixture
 def order_of_service_with_declaration():
     pass
 
 
-# @pytest.fixture
+@pytest.fixture
 def order_of_service_without_declaration() -> list[tuple[str, int, str]]:
     """
     Order of service with elements at position:
@@ -41,8 +47,10 @@ def order_of_service_without_declaration() -> list[tuple[str, int, str]]:
     ]
 
 
-# @pytest.fixture
-def clean_order_of_service_without_declaration() -> list[tuple[str, str]]:
+@pytest.fixture
+def clean_order_of_service_without_declaration(
+    order_of_service_without_declaration: list[tuple[str, int, str]]
+) -> list[tuple[str, str]]:
     """
     Order of service with elements at position:
     1. Item
@@ -53,8 +61,8 @@ def clean_order_of_service_without_declaration() -> list[tuple[str, str]]:
     """
     return [
         (section_mapping().get(item[0], item[0]), item[2])
-        for item in order_of_service_without_declaration()
-        if not ("Opening Words" in item[0] or "Dismissal" in item[0])
+        for item in order_of_service_without_declaration
+        if not re.match("(Opening Words|Dismissal|Closing Words)", item[0])
     ]
 
 
@@ -72,14 +80,14 @@ def sermon_discussion_qns():
     return
 
 
-# @pytest.fixture
+@pytest.fixture
 def all_slides() -> list[Slide]:
     return get_all_slides(pptx=pptx())
 
 
 def pptx() -> Presentation:
-    PATH_TO_PRESENTATION = Path("../input/01.05 (9am) service slides.pptx")
-    # PATH_TO_PRESENTATION = Path("input/01.05 (9am) service slides.pptx")
+    # PATH_TO_PRESENTATION = Path("../input/01.05 (9am) service slides.pptx")
+    PATH_TO_PRESENTATION = Path("input/01.05 (9am) service slides.pptx")
     return Presentation(PATH_TO_PRESENTATION)
 
 
@@ -108,7 +116,7 @@ def get_slide_subset_with_text(all_slides: list[Slide], text: str) -> list[Slide
     return section_headers
 
 
-# @pytest.fixture
+@pytest.fixture
 def section_headers(all_slides: list[Slide]) -> list[Slide]:
     """
     Section header slides are slides that contain the text "Today's order of service".
@@ -128,10 +136,6 @@ def get_text_from_slides(slides: list[Slide]) -> list[str]:
     return [shape.text_frame.text for slide in slides for shape in slide.shapes]
 
 
-def test_existence_of_section_headers(section_headers: list[Slide]):
-    assert len(section_headers) > 0
-
-
 def split_and_strip(text: str) -> list[str]:
     split_text = re.split("\n+", text)
     return [
@@ -141,7 +145,7 @@ def split_and_strip(text: str) -> list[str]:
     ]
 
 
-def get_order_of_service_in_slide(text: str) -> list[tuple[str, str]]:
+def order_of_service_in_slide(text: str) -> list[tuple[str, str]]:
     return [item for item in text if "order of service" in item]
 
 
@@ -149,16 +153,20 @@ def get_reformatted_text(text: str) -> list[tuple[str, str]]:
     return [split_and_strip(item) for item in text]
 
 
-def get_final():
-    section_header_slides = section_headers(all_slides())
-    section_header_text = get_text_from_slides(section_header_slides)
-    orders_of_service = get_order_of_service_in_slide(section_header_text)
+@pytest.fixture
+def clean_order_of_service_in_slides(section_headers: list[Slide]):
+    section_header_text = get_text_from_slides(section_headers)
+    orders_of_service = order_of_service_in_slide(section_header_text)
     return [split_and_strip(item) for item in orders_of_service]
 
 
+def test_existence_of_section_headers(section_headers: list[Slide]):
+    assert len(section_headers) > 0
+
+
 def test_section_headers_have_correct_order(
-    order_of_service_without_declaration: tuple[str, int, str],
-    section_headers: list[Slide],
+    clean_order_of_service_without_declaration: tuple[str, str],
+    clean_order_of_service_in_slides: list[tuple[str, str]],
 ):
     """
     Test all section headers have the correct order of service by checking that the
@@ -171,11 +179,25 @@ def test_section_headers_have_correct_order(
     # 1. Identify the section header slides
     # 2. Check that there is a text box on the page that contains the order of service
     # 3. Check that the order of service is correct
-    pass
+    for slide in clean_order_of_service_in_slides:
+        index = 0
+        for entry in slide:
+            if (
+                re.match("(Opening Song|Closing Song|Hearing God\u2018s Word Read)", entry)
+                is not None
+            ):
+                title, comments = clean_order_of_service_without_declaration[index]
+                assert f"{title} \u2013 {comments}" == entry
+            elif entry != clean_order_of_service_without_declaration[index][0]:
+                continue
+            index += 1
+        assert index == len(
+            clean_order_of_service_without_declaration
+        ), "The order of service in the slides do not contain the entire required order of service!"
 
 
 def test_family_confession_content_matches_number(
-    order_of_service_without_declaration: tuple[str, int, str]
+    clean_order_of_service_without_declaration: tuple[str, str]
 ):
     """
     Test family confession has the correct contents by checking that the words on the
@@ -191,7 +213,7 @@ def test_family_confession_content_matches_number(
 
 
 def test_family_declaration_content_matches_number(
-    order_of_service_without_declaration: tuple[str, int, str],
+    clean_order_of_service_without_declaration: tuple[str, str],
 ):
     """
     Test that family declaration (if present) has the correct contents by checking that
@@ -204,7 +226,7 @@ def test_family_declaration_content_matches_number(
 
 
 def test_all_lyric_slides_have_no_title(
-    order_of_service_without_declaration: tuple[str, int, str]
+    clean_order_of_service_without_declaration: tuple[str, str]
 ):
     """
     Test all lyric slides do not contain a title.
